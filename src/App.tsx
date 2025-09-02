fdbd5997534945ef1769bfd7f642269deeb3776d
Arnab <arnabmandal661@gmail.com>
2025-09-02 15:36:15 +0530

    changes in html

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
     return (
         <>
             <h1 className="text-3xl font-bold underline">
-                mood tracker
+                mood tracker 2
             </h1>
         </>)
 }
